8639f4110c005d029c88a1a8063e5720f0a6fffa
Blake Anderson <bganderson95@gmail.com>
2020-03-20 12:16:33 -0400

    config

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import SideDrawer from './Components/Navigation/SideDrawer/SideDrawer';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <SideDrawer/>
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to Blake App</h1>
